12daa6beb04b3e7387376d995733eb771242fadd
Feras-ALdemashki <feras.aldemashki@hotmail.com>
2025-05-20 18:00:09 +0200

    create Navbar with some dummy testing

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -2,31 +2,30 @@ import { Link } from "react-router-dom";
 import "../styles/Navbar.css";
 import GuestImage from "../assets/Guest-image.jpg";
 import userImage from "../assets/user.jpg";
-const user = false;
+// this is for test if user logged in or not
+const user = true;
 const Nav = () => {
   return (
     <div className="Navbar">
-      {user ? <p> user name</p> : <p>Guest</p>}
-      <ul>
-        {user && (
+      {user ? <p> Reyna Conrad </p> : <p>App name</p>}
+      {user && (
+        <ul>
           <Link to="/">
             {" "}
             <li>Home</li>
           </Link>
-        )}
-        {user && (
+
           <Link to="/">
             {" "}
             <li>Create trip</li>
           </Link>
-        )}
-        {user && (
+
           <Link to="/">
             {" "}
             <li>Profile</li>
           </Link>
-        )}
-      </ul>
+        </ul>
+      )}
       <div className=" Auth-navbar">
         {" "}
         {user ? <Link to="/"> Log out</Link> : <Link to="/"> Login</Link>}
