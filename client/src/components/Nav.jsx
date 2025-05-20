8218d246a54d7b974da4419c8d897288f40ee984
Feras-ALdemashki <feras.aldemashki@hotmail.com>
2025-05-20 17:25:28 +0200

    created the navbar

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,17 +1,42 @@
 import { Link } from "react-router-dom";
-
-import TEST_ID from "./Nav.testid";
-
+import "../styles/Navbar.css";
+import GuestImage from "../assets/Guest-image.jpg";
+import userImage from "../assets/user.jpg";
+const user = false;
 const Nav = () => {
   return (
-    <ul>
-      <Link to="/" data-testid={TEST_ID.linkToHome}>
-        <li>Home</li>
-      </Link>
-      <Link to="/user" data-testid={TEST_ID.linkToUsers}>
-        <li>Users</li>
-      </Link>
-    </ul>
+    <div className="Navbar">
+      {user ? <p> user name</p> : <p>Guest</p>}
+      <ul>
+        {user && (
+          <Link to="/">
+            {" "}
+            <li>Home</li>
+          </Link>
+        )}
+        {user && (
+          <Link to="/">
+            {" "}
+            <li>Create trip</li>
+          </Link>
+        )}
+        {user && (
+          <Link to="/">
+            {" "}
+            <li>Profile</li>
+          </Link>
+        )}
+      </ul>
+      <div className=" Auth-navbar">
+        {" "}
+        {user ? <Link to="/"> Log out</Link> : <Link to="/"> Login</Link>}
+        {user ? (
+          <img src={userImage} alt="user pic" />
+        ) : (
+          <img src={GuestImage} alt="Guest pic" />
+        )}
+      </div>
+    </div>
   );
 };
 
