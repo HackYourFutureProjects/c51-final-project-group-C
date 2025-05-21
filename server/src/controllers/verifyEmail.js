19ca6fb3892d4ab276569c7e6f9ee010d2859518
sertanrdn <sertanerdogan07@gmail.com>
2025-05-21 09:30:35 +0200

    Addied the verify email logic that has sent to the user

---
+++ server/src/controllers/verifyEmail.js
@@ -0,0 +1,22 @@
+import User from "../models/User";
+
+export async function verifyEmail(req, res) {
+  const { token } = req.body;
+
+  if (!token) return res.status(400).send("Token is missing.");
+
+  try {
+    const user = await User.findOne({ verificationToken: token });
+
+    if (!user) return res.status(400).send("Invalid or expired token.");
+
+    user.isVerified = true;
+    user.verificationToken = undefined;
+    await user.save();
+
+    res.send("Email successfully verified! You can now log in.");
+  } catch {
+    // console.error(err);
+    res.status(500).send("Server error");
+  }
+}
