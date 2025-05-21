cd1385836bbca114f5038850e1bdc91c167ec9ff
sertanrdn <sertanerdogan07@gmail.com>
2025-05-21 09:46:01 +0200

    Defined the routes for registiration

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,9 +1,15 @@
 import express from "express";
 import { createUser, getUsers } from "../controllers/user.js";
+import { authController } from "../controllers/authController.js";
+import { verifyEmail } from "../controllers/verifyEmail.js";
+import { completeProfile } from "../controllers/completeProfile.js";
 
 const userRouter = express.Router();
 
 userRouter.get("/", getUsers);
 userRouter.post("/", createUser);
+userRouter.post("/register", authController);
+userRouter.post("/verify-email", verifyEmail);
+userRouter.post("/complete-profile", completeProfile);
 
 export default userRouter;
