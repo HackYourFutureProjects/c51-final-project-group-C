00cd22f3492eb159e95993f869767c97c84ea3e3
gsaw01 <glib.sawchenko@gmail.com>
2025-05-24 10:27:40 +0200

    Added login/logout routes

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,15 +1,16 @@
 import express from "express";
-// import { createUser, getUsers } from "../controllers/user.js";
 import { registerUser } from "../controllers/auth/register.js";
 import { verifyEmail } from "../controllers/auth/verifyEmail.js";
 import { completeProfile } from "../controllers/auth/completeProfile.js";
+import { login } from "../controllers/auth/login.js";
+import { logout } from "../controllers/auth/logout.js";
 
 const authRouter = express.Router();
 
-// userRouter.get("/", getUsers);
-// userRouter.post("/", createUser);
 authRouter.post("/register", registerUser);
 authRouter.post("/verify-email", verifyEmail);
 authRouter.post("/complete-profile", completeProfile);
+authRouter.post("/login", login);
+authRouter.post("/logout", logout);
 
 export default authRouter;
