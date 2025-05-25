ed4aaeda15084a01ea0d98bf530fe9109c7e4fe9
sertanrdn <sertanerdogan07@gmail.com>
2025-05-25 18:25:39 +0200

    Created logic for forgot password and reset password

---
+++ server/src/controllers/auth/forgotPassword.js
@@ -0,0 +1,33 @@
+import User from "User";
+import crypto from "crypto";
+import { sendResetPassEmail } from "../../util/sendResetPassEmail";
+import { logError } from "../../util/logging";
+
+export async function forgotPassword(req, res) {
+  const { email } = req.body;
+
+  if (!email) {
+    return res.status(400).json({ message: "Email is required!" });
+  }
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "This email does not exists." });
+    }
+
+    const resetPassToken = crypto.randomBytes(32).toString("hex");
+    const resetPassTokenExpiresAt = new Date(Date.now() + 60 * 60 * 1000);
+
+    user.resetPassToken = resetPassToken;
+    user.resetPassTokenExpiresAt = resetPassTokenExpiresAt;
+    await user.save();
+
+    await sendResetPassEmail(email, resetPassToken);
+
+    res.json({ message: "Please check your email to reset your password" });
+  } catch (err) {
+    logError(err);
+    res.status(500).json({ message: "Server error." });
+  }
+}
---
+++ server/src/controllers/auth/resetPassword.js
@@ -0,0 +1,48 @@
+import User from "../../models/User";
+import bcrypt from "bcrypt";
+import { logError } from "../../util/logging";
+
+export async function resetPassword(req, res) {
+  const { token } = req.params;
+  const { password } = req.body;
+
+  if (!token || !password) {
+    return res.status(400).json({ message: "Missing token or password." });
+  }
+
+  try {
+    const user = await User.findOne({ resetPassToken: token });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid token" });
+    }
+
+    if (
+      !user.resetPassTokenExpiresAt ||
+      new Date() > user.resetPassTokenExpiresAt
+    ) {
+      return res.status(400).json({ message: "Token has expired." });
+    }
+
+    if (password.length < 8) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 8 characters." });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    user.password = hashedPassword;
+    user.resetPassToken = undefined;
+    user.resetPassTokenExpiresAt = undefined;
+
+    await user.save();
+
+    res
+      .status(200)
+      .json({ message: "Password has been changed successfully." });
+  } catch (err) {
+    logError(err);
+    res.status(500).json({ message: "Server error." });
+  }
+}
