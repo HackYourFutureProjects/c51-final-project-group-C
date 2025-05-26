dbbadbbad05aa7d78359ce00db609b56f347916e
gsaw01 <glib.sawchenko@gmail.com>
2025-05-27 00:26:57 +0200

    add cors origin from env

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,13 @@ import authRouter from "./routes/authRoutes.js";
 const app = express();
 
 // Enable CORS for the frontend
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  }),
+);
+
 app.use(cookieParser());
 
 // Tell express to use the json middleware
