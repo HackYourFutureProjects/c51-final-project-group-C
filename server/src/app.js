3f757de194889678e7225d500df2dbc0351eba0b
Feras-ALdemashki <feras.aldemashki@hotmail.com>
2025-05-26 17:15:58 +0200

    add backend logic for creating trips and fetching countries

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/authRoutes.js";
+import tripRouter from "./routes/tripRoutes.js";
+import countryRouter from "./routes/countryRoutes.js";
 
 // Create an express server
 const app = express();
@@ -19,5 +21,7 @@ app.use(express.json());
  * As we also host our client code on heroku we want to separate the API endpoints.
  */
 app.use("/api/auth", authRouter);
+app.use("/api/trip", tripRouter);
+app.use("/api/country", countryRouter);
 
 export default app;
