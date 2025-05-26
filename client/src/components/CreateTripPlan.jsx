a649d5b8f0b0d049c935e4bca201749902f161d8
sertanrdn <sertanerdogan07@gmail.com>
2025-05-26 18:42:38 +0200

    Installed react-select to use for countries options and updated the CreateTripPlan file to use that module

--- client/src/components/CreateTripPlan.jsx
+++ client/src/components/CreateTripPlan.jsx
@@ -1,19 +1,74 @@
-import { useState } from "react";
-// import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Select from "react-select";
 
 const CreateTripPlan = () => {
   const [title, setTitle] = useState("");
-  const [country, setCountry] = useState("");
+  const [countries, setCountries] = useState([]);
+  const [selectedCountries, setSelectedCountries] = useState([]);
   const [duration, setDuration] = useState("");
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    const fetchCountries = async () => {
+      try {
+        const res = await fetch("/api/country/get-countries", {
+          credentials: "include",
+        });
+
+        const data = await res.json();
+
+        // Converting the data to react-select format
+        const formattedData = data.map((country) => ({
+          value: country._id,
+          label: country.name,
+        }));
+        setCountries(formattedData);
+      } catch (err) {
+        console.error("Failed to fetch countries: ", err);
+      }
+    };
+
+    fetchCountries();
+  }, []);
+
+  // Submit the form to start creating the trip
   const handleSubmit = async (e) => {
     e.preventDefault();
+    try {
+      const countryIds = selectedCountries.map((c) => c.value);
 
-    // TODO: Actual API call
+      const res = await fetch("/api/trip/create-trip", {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title,
+          duration: Number(duration),
+          countries: countryIds,
+        }),
+      });
 
-    // Redirect to Trip Details Page with navigate
+      if (!res.ok) {
+        throw new Error("Something went wrong");
+      }
+
+      const newTrip = await res.json();
+
+      // Redirect the users to the Trip Details Page with navigate
+      navigate(`/trip/trip-details/${newTrip._id}`);
+    } catch (err) {
+      console.error("Error while creating trip: ", err.message);
+    }
   };
+
+  // const handleCountryChange = (e) => {
+  //   const selectedOptions = Array.from(e.target.selectedOptions);
+  //   const selectedIds = selectedOptions.map((opt) => opt.value);
+  //   setCountryIds(selectedIds);
+  // };
 
   return (
     <form onSubmit={handleSubmit} className="max-w-md mx-auto space-y-4">
@@ -28,11 +83,14 @@ const CreateTripPlan = () => {
         />
       </div>
       <div>
-        <label>Country:</label>
-        <input
-          value={country}
-          className="w-full border rounded p-2"
-          onChange={(e) => setCountry(e.target.value)}
+        <label>Countries:</label>
+        <Select
+          isMulti
+          options={countries}
+          value={selectedCountries}
+          className="w-full"
+          onChange={setSelectedCountries}
+          classNamePrefix="react-select"
           required
         />
       </div>
