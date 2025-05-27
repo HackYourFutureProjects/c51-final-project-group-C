b23801711f35301347b7b90cb641831ff8bc1dd5
Feras-ALdemashki <feras.aldemashki@hotmail.com>
2025-05-27 14:58:08 +0200

    Update countryRoutes.js
    changed the endpoint from get-countries to countries

--- server/src/routes/countryRoutes.js
+++ server/src/routes/countryRoutes.js
@@ -3,6 +3,6 @@ import { getAllCountries } from "../controllers/country/getAllCountries.js";
 
 const countryRouter = express.Router();
 
-countryRouter.get("/get-countries", getAllCountries);
+countryRouter.get("/countries", getAllCountries);
 
 export default countryRouter;
