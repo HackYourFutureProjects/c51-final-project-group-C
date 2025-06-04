027485587fe7dfe6637811bc8b994f6603d52a1e
Feras-ALdemashki <feras.aldemashki@hotmail.com>
2025-06-04 15:29:59 +0200

    added a controller to get countries by id

--- server/src/routes/countryRoutes.js
+++ server/src/routes/countryRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { getAllCountries } from "../controllers/country/getAllCountries.js";
+import { getCountryById } from "../controllers/country/getCountriesById.js";
 
 const countryRouter = express.Router();
 
 countryRouter.get("/", getAllCountries);
+countryRouter.get("/:id", getCountryById);
 
 export default countryRouter;
