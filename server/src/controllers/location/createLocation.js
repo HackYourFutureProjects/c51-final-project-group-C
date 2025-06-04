691b22f7640e148c342a67e1b8eb3f64232d3710
Feras-ALdemashki <feras.aldemashki@hotmail.com>
2025-06-04 16:56:07 +0200

    updating the location controller and added location validation

--- server/src/controllers/location/createLocation.js
+++ server/src/controllers/location/createLocation.js
@@ -1,9 +1,17 @@
 import Location from "../../models/Location.js";
-
+import Activity from "../../models/Activity.js";
 import { logError } from "../../util/logging.js";
 
 export const createLocation = async (req, res) => {
+  //    example of the body {
+  //   "coordinates": {
+  //     "lat": 40.7128,
+  //     "lng": -74.0060
+  //   },
+  //   "address": "123 Main St, New York, NY"
+  // }
   const { coordinates, address } = req.body;
+  const { activityID } = req.params;
   const userID = req.user.userId;
 
   try {
@@ -12,7 +20,15 @@ export const createLocation = async (req, res) => {
       address,
       userID,
     });
+
     const savedLocation = await newLocation.save();
+
+    if (activityID) {
+      await Activity.findByIdAndUpdate(activityID, {
+        location: savedLocation._id,
+      });
+    }
+
     res.status(200).json(savedLocation);
   } catch (err) {
     res
--- server/src/routes/locationRoutes.js
+++ server/src/routes/locationRoutes.js
@@ -1,9 +1,16 @@
 import express from "express";
 import { createLocation } from "../controllers/location/createLocation.js";
 import { requireAuth } from "../middleware/requireAuth.js";
+import { validate } from "../middleware/validateData.js";
+import { createLocationSchema } from "../validation/schemas/locationSchemas.js";
 
 const locationRouter = express.Router({ mergeParams: true });
 
-locationRouter.post("/create-location", requireAuth, createLocation);
+locationRouter.post(
+  "/create-location",
+  requireAuth,
+  validate(createLocationSchema),
+  createLocation,
+);
 
 export default locationRouter;
