84bd16a86410833a0794019696c77e37525b12ca
Feras-ALdemashki <feras.aldemashki@hotmail.com>
2025-06-05 00:59:23 +0200

    fix some naming

--- server/src/routes/countryRoutes.js
+++ server/src/routes/countryRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { getAllCountries } from "../controllers/country/getAllCountries.js";
-import { getCountryById } from "../controllers/country/getCountriesById.js";
+import { getCountryById } from "../controllers/country/getCountryById.js";
 
 const countryRouter = express.Router();
 
