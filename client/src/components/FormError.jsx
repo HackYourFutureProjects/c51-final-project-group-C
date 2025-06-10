c6f8cbdb97f45b9c26bd73a3bdea1433b37db171
gsaw01 <glib.sawchenko@gmail.com>
2025-06-10 13:43:26 +0200

    update existing components/pages for photo upload

--- client/src/components/FormError.jsx
+++ client/src/components/FormError.jsx
@@ -1,8 +1,7 @@
 const FormError = ({ message }) => {
   if (!message) return null;
-
   return (
-    <div className="form-error text-red-500 text-md text-center bg-background flex items-center justify-center p-2 rounded-lg border border-accent">
+    <div className="form-error text-[#bd5151] text-sm text-center bg-[#e3c5c5] flex items-center justify-center p-2 rounded-lg border border-[#bd5151]">
       {message}
     </div>
   );
