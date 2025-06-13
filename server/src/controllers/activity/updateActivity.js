c13d1829402e9ca9362a0ea2c1a6dd05e5c4c069
sertanrdn <sertanerdogan07@gmail.com>
2025-06-13 12:35:46 +0200

    Created updating activity controller

---
+++ server/src/controllers/activity/updateActivity.js
@@ -0,0 +1,26 @@
+import Activity from "../../models/Activity";
+import { logError } from "../../util/logging";
+
+export const updateActivity = async (req, res) => {
+  const { activityID } = req.params;
+  const { name, price, notes } = req.body;
+
+  try {
+    const updatedActivity = await Activity.findByIdAndUpdate(
+      activityID,
+      { name, price, notes },
+      { new: true, runValidators: true },
+    );
+
+    if (!updatedActivity) {
+      return res.status(404).json({ error: "Activity not found" });
+    }
+
+    res.status(200).json(updatedActivity);
+  } catch (err) {
+    res
+      .status(err.status || 500)
+      .json({ errors: err.errors || "Server error" });
+    logError(err);
+  }
+};
